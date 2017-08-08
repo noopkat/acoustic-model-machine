13a6311c3375e3c9f68c128be250b3cf20d0736a
Suz Hinton <suz.hinton@gmail.com>
2017-08-07 23:51:36 -0400

    abstract generator functionality

--- bin/acoustic-model-machine.js
+++ bin/acoustic-model-machine.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const path = require('path');
 const minimist = require('minimist');
-const srtToObj = require('srt-to-obj');
-const eachSeries = require('async/eachSeries');
 const colors = require('colors/safe');
-const ProgressBar = require('progress');
 
-const processor = require('../lib/processor');
+const util = require('../lib/util');
 const help = require('../lib/help');
-
+const generator = require('../lib/generator');
 
 /*
  example use:
@@ -26,63 +22,6 @@ if (argv.help) {
   process.exit();
 }
 
-const generateDataset = () => {
- const subtitlePath = path.resolve(opts.subtitle);
-
- srtToObj(subtitlePath)
-   .then(onParseSuccess)
-   .catch(onParseError);
-};
-
-const onParseSuccess = (data) => {
- console.log('generating dataset...');
-
- let progressBar;
- if (!opts.v && !opts.verbose) {
-   progressBar = createProgressBar(data.length);
- };
-
- eachSeries(data, onSubtitle(progressBar), onSubtitleComplete);
-};
-
-const onParseError = (reason) => {
-  console.error(colors.red(reason));
-  process.exit(9);
-};
-
-const onSubtitle = (progressBar) => (sub, next) => {
-  if (progressBar) progressBar.tick();
-
-  const transcription = processor.processSubtitle(sub, opts);
-  transcriptionFile.write(transcription);
-
-  processor.createAudioSample(sub, opts, next);
-};
-
-const onSubtitleComplete = (error) => {
-  transcriptionFile.end();
-
-  if (error) {
-    console.error(colors.red(error));
-    process.exit(9);
-  } else {
-    console.log(colors.green('complete.'));
-    process.exit();
-  }
-};
-
-const createProgressBar = (total) => {
-  const options = {
-    total: total,
-    width: 70,
-    complete: colors.cyan('='),
-    incomplete: colors.grey('-')
-  };
-
-  return progressBar = new ProgressBar('[:bar] :current / :total', options);
-};
-
-
 // set up defaults
 const defaultArgs = {
   'output-prefix': 'speech',
@@ -93,18 +32,15 @@ const defaultArgs = {
 // merge default args with supplied args
 const opts = Object.assign({}, defaultArgs, argv);
 
-// create stream for transcription file
-const transcriptionFile = fs.createWriteStream(path.resolve(opts['script-output']));
-
 // check for subtitle and source first
-processor.findInvalidOptions(opts, (error, list) => {
+util.findInvalidOptions(opts, (error, list) => {
   if (list.length) {
     console.error(colors.red(list.join('\n')));
     process.exit(9);
   }
 
   // parse subtitle file and generate transcription / audio files
-  generateDataset();
+  generator.generateDataset(opts);
 });
 
 
